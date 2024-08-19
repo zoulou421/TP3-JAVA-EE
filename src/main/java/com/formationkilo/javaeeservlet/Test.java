7c59ac5584ad9bb7d0df79a52905715ce4d840c0
Bonevy BEBY <bonevybeby@gmail.com>
2024-08-19 08:43:04 +0200

    first commit

---
+++ src/main/java/com/formationkilo/javaeeservlet/Test.java
@@ -0,0 +1,44 @@
+package com.formationkilo.javaeeservlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/test")
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public Test() {
+     
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		
+		PrintWriter out=response.getWriter();
+		//out.println("Bonjour");
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset=\"utf-8\" />");
+		out.println("<title>Servlet</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Hello from Servlet!</h1>");
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+}
