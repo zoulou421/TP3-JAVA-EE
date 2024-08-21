ca9e1484068a3acb28097c54c656bbfdd13a5d9f
Bonevy BEBY <bonevybeby@gmail.com>
2024-08-21 04:47:39 +0200

    add menus

--- src/main/java/com/formationkilo/javaeeservlet/Test.java
+++ src/main/java/com/formationkilo/javaeeservlet/Test.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/test")
+//@WebServlet("/test")
 public class Test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
    
--- src/main/webapp/WEB-INF/menu.jsp
+++ src/main/webapp/WEB-INF/menu.jsp
@@ -2,7 +2,7 @@
     pageEncoding="ISO-8859-1"%>
 
  <ul>
-    <li>page 1</li>
-    <li>page 1</li>
+    <li><a href="/javaeeservlet/Home">Accueil</a></li>
+    <li><a href="/javaeeservlet/Test">Page JSP</a></li>
     <li>page 1</li>
  </ul>
